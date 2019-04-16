146930d3b525adabc7766cb718e8ed7bdf01d9b1
larostime <mymail166944@gmail.com>
2019-04-16 14:54:26 +0300

    4st(2)

--- calculator.py
+++ calculator.py
@@ -1,9 +1,12 @@
 def add(x, y):
    return x + y
+
 def subtract(x, y):
    return x - y
+
 def multiply(x, y):
    return x * y
+
 def divide(x, y):
    return x / y
 
@@ -20,10 +23,13 @@ num2 = int(input("Enter second number: "))
 
 if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))
+
 elif choice == '2':
    print(num1,"-",num2,"=", subtract(num1,num2))
+
 elif choice == '3':
    print(num1,"*",num2,"=", multiply(num1,num2))
+
 elif choice == '4':
    print(num1,"/",num2,"=", divide(num1,num2))
 else:
