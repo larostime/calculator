2c7c8703f33203f143980624120a54cc521c324c
larostime <mymail166944@gmail.com>
2019-04-16 14:40:43 +0300

    2st

--- calculator.py
+++ calculator.py
@@ -1,15 +1,20 @@
 def add(x, y):
    return x + y
+def subtract(x, y):
+   return x - y
 
 print("Select operation.")
 print("1.Add")
+print("2.Subtract")
 
-choice = input("Enter choice(1):")
+choice = input("Enter choice(1/2):")
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
 else:
    print("Invalid input")
